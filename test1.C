22d25b229105aea20a3d9d6418aca4a09e23bc62
Ravi Janjam <ravikishorejnjm@gmail.com>
2015-10-29 13:57:34 -0500

    test updated

--- test1.C
+++ test1.C
@@ -6,12 +6,35 @@ using namespace std;
 struct dat{
 
 	vector<int> vv;
-	map<string,vector<int>> mp;
+	map<char ,double> mp;
 }sD;
 
 void test1(){
 
-//	vector<int> vv; 
+
+	string a2z = "ABCDEFGHIJKLMNOPQRXYZA";
+
+	vector<char> vss;
+
+	double i=0; 	
+	for (char c : a2z) 
+	{ 
+		vss.push_back(c);
+		cout << "from inside vector: " << vss.at(i) << endl;
+		sD.mp[c] = i++;
+		cout << c << endl;
+	}
+
+	for( int i=0; i <= vss.size() ; i++ ){
+
+		string pp;
+		pp += vss.at(i);
+		pp += vss.at(i+1);
+		cout << "strings together" << pp << endl;
+
+	}
+	
+	for (int i=0, j=0; i < 10 && j<10; i++, j++) cout << i << "," << j << endl;
 	cout << "Filling sequence " << endl;
 	for (int i=0; i< 5; i++) {sD.vv.push_back(i); cout << i;}
 	cout << endl;
@@ -19,6 +42,10 @@ void test1(){
 	cout << "Retrieving sequence " << endl;
 	for (int i=0; i< 5; i++) cout << sD.vv.at(i) ;
 	cout << endl;
+
+	map<string, string> nm;
+	nm["name"] = "Hen";
+	nm["type"] = "Bird";
 }
 
 /*
